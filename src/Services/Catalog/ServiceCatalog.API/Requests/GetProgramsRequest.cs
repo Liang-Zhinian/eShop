3bc68013a741c1cc4e729e03240a956a20955f59
Liang-Zhinian <32760516+Liang-Zhinian@users.noreply.github.com>
2018-08-09 11:07:38 +0800

    bug fix

---
+++ src/Services/Catalog/ServiceCatalog.API/Requests/GetProgramsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Registration.Api.Requests
+{
+    public class GetProgramsRequest:BaseRequest
+    {
+        public GetProgramsRequest()
+        {
+        }
+
+        public string /* or class ScheduleType */ ScheduleType { get; set; }
+        public bool OnlineOnly { get; set; }
+    }
+}
---
+++ src/Services/Catalog/ServiceCatalog.API/Requests/GetServiceItemsRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Registration.Api.Requests
+{
+    public class GetServiceItemsRequest : BaseRequest
+    {
+        public GetServiceItemsRequest()
+        {
+        }
+
+        public Registration.Domain.ReadModel.Program[] ProgramIds { get; set; }
+        public bool OnlineOnly { get; set; }
+    }
+}
