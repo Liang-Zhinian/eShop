5479cd424e1c8c966e512245950ed77eac274b17
liang-zhinian <a13533550310@live.com>
2018-10-10 11:33:23 +0800

    Revert "add staff schedule screen"
    This reverts commit 690f502323ac5f547cef9f7ece3ae3573821ce4a.

--- src/Mobile/Book2Business/Src/Native/Containers/Appointments/ServiceItemListing.js
+++ src/Mobile/Book2Business/Src/Native/Containers/Appointments/ServiceItemListing.js
@@ -7,7 +7,11 @@ import { Icon } from 'native-base'
 
 import { Images } from '../../Themes'
 <<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Containers/Appointments/ServiceItemListing.js
+<<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Containers/Appointments/ServiceItemListing.js
 import { setSelectedServiceItem, getServiceItems, setError } from '../../../Actions/serviceItems'
+=======
+import { getServiceItems, setError } from '../../Actions/serviceItems'
+>>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Containers/Appointments/ServiceItemListing.js
 =======
 import { getServiceItems, setError } from '../../Actions/serviceItems'
 >>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Containers/Appointments/ServiceItemListing.js
--- src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
+++ src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
@@ -4,8 +4,12 @@ import AppointmentCategoryListingScreen from '../../Containers/Appointments/Serv
 import AppointmentListingScreen from '../../Containers/Appointments/ServiceItemListing'
 import AppointmentTypeScreen from '../../Containers/Appointments/Appointment'
 <<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
+<<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
 import StaffScheduleListingScreen from '../../Containers/StaffSchedules/StaffScheduleListing'
 import StaffScheduleScreen from '../../Containers/StaffSchedules/StaffSchedule'
+=======
+import CreateStackNavigator from '../CreateStackNavigator'
+>>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Navigation/Stacks/AppointmentsStack.js
 =======
 import CreateStackNavigator from '../CreateStackNavigator'
 >>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Navigation/Stacks/AppointmentsStack.js
@@ -15,8 +19,11 @@ export default CreateStackNavigator({
   AppointmentListing: { screen: AppointmentListingScreen },
   AppointmentType: { screen: AppointmentTypeScreen },
 <<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
+<<<<<<< HEAD:src/Mobile/Book2Business/Src/Native/Navigation/Stacks/AppointmentsStack.js
   StaffScheduleListing: {screen: StaffScheduleListingScreen},
   StaffSchedule: {screen: StaffScheduleScreen}
+=======
+>>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Navigation/Stacks/AppointmentsStack.js
 =======
 >>>>>>> parent of 690f502... add staff schedule screen:src/Mobile/Book2Business/App/Navigation/Stacks/AppointmentsStack.js
 }, 'screen')
