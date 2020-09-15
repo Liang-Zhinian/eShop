a2392046bb289fd9de907e97fa6e7662fabc76e6
Liang Zhinian <a13533550310@live.com>
2020-09-15 21:08:57 +0800

    merge from b408d58 on 17 Dec 2018

--- src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
+++ src/Services/Ordering/Ordering.Domain/AggregatesModel/OrderAggregate/OrderStatus.cs
@@ -12,8 +12,6 @@
         public static OrderStatus Submitted = new OrderStatus(1, nameof(Submitted).ToLowerInvariant());
         public static OrderStatus AwaitingValidation = new OrderStatus(2, nameof(AwaitingValidation).ToLowerInvariant());
         public static OrderStatus StockConfirmed = new OrderStatus(3, nameof(StockConfirmed).ToLowerInvariant());
-        // TODO: OrderStatus - on-hold (we're awaiting the payment).
-
         public static OrderStatus Paid = new OrderStatus(4, nameof(Paid).ToLowerInvariant());
         public static OrderStatus Shipped = new OrderStatus(5, nameof(Shipped).ToLowerInvariant());
         public static OrderStatus Cancelled = new OrderStatus(6, nameof(Cancelled).ToLowerInvariant());
