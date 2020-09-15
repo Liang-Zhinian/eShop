bfe707a27c3ef7a40629ce9da0de8ac6ad3d7e20
Liang Zhinian <a13533550310@live.com>
2020-09-15 20:55:39 +0800

    merge from b408d58 on 17 Dec 2018

--- src/Services/Identity/Identity.API/Services/ILoginService.cs
+++ src/Services/Identity/Identity.API/Services/ILoginService.cs
@@ -1,11 +1,16 @@
 ﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 
 namespace Eva.eShop.Services.Identity.API.Services
 {
     public interface ILoginService<T>
     {
         Task<bool> ValidateCredentials(T user, string password);
+
         Task<T> FindByUsername(string user);
+
         Task SignIn(T user);
+
+        Task SignInAsync(T user, AuthenticationProperties properties, string authenticationMethod = null);
     }
 }
